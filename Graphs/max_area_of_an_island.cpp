2f99bef11ed827e51e9250500356c63a127ff370
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-06-27 06:16:05 +0300

    Solve max area of island problem

---
+++ Graphs/max_area_of_an_island.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int currentCount = 0;
+int maxCount = 0;
+
+void DFS(int r, int c, vector<vector<int>> &mat){
+    int rows = mat.size();
+    int col = mat[0].size();
+
+    if(r < 0 || c < 0 || r >= rows || c >= col){
+        return;
+    }
+
+    if(mat[r][c] == 0){
+        return;
+    }
+
+    currentCount++;
+    mat[r][c] = 0;
+
+    DFS(r + 1, c, mat);
+    DFS(r - 1, c, mat);
+    DFS(r, c + 1, mat);
+    DFS(r, c - 1, mat);
+
+}
+
+int maxAreaOfIsland(vector<vector<int>>& grid) {
+    for(int i = 0; i < grid.size(); i++){
+        for(int j = 0; j < grid[0].size(); j++){
+            if(grid[i][j] == 1){
+                currentCount = 0;
+                DFS(i, j, grid);
+                if(currentCount > maxCount){
+                    maxCount = currentCount;
+                }
+            }
+        }
+    }
+    return maxCount;
+}
+
+int main(){
+    vector<vector<int>> grid = {
+        {0,1,1,0,1},
+        {1,0,1,0,1},
+        {0,1,1,0,1},
+        {0,1,0,0,1}
+    };
+
+    cout << maxAreaOfIsland(grid);
+}

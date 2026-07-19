dc4ae818878379131d46f62a1cfdc3a1cba4269f
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-07-19 14:08:54 +0200

    Solve invert a binary tree

---
+++ Trees/invert_binary_tree.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(!root){
+            return NULL;
+        }
+
+        invertTree(root->left);
+        invertTree(root->right);
+
+        swap(root->left, root->right);
+
+        return root;
+        
+    }
+};

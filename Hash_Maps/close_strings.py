ab76c0cff1822254f044c5ac6d1ed144e24d0223
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-09-12 15:01:43 +0300

    Solve close strings

---
+++ Hash_Maps/close_strings.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+class Solution:
+    def closeStrings(self, word1: str, word2: str) -> bool:
+        count1 = Counter(word1)
+        count2 = Counter(word2)
+
+        if set(count1.keys()) != set(count2.keys()):
+            return False
+
+        return Counter(count1.values()) == Counter(count2.values())

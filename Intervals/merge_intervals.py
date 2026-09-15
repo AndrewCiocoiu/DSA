2866e4843befec246eb9034a4f885a026f666a8a
AndrewCiocoiu <andrewciocoiu@gmail.com>
2026-09-15 23:21:23 +0300

    Solve merge intervals

---
+++ Intervals/merge_intervals.py
@@ -0,0 +1,13 @@
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        merged_intervals = []
+
+        intervals.sort(key=lambda x:x[0])
+        
+        for interval in intervals:
+            if not merged_intervals or merged_intervals[-1][1] < interval[0]:
+                merged_intervals.append(interval)
+            else:
+                merged_intervals[-1][1] = max(merged_intervals[-1][1], interval[1])
+
+        return merged_intervals
